ce35080ce07776c699ad6d03e63a56554bc0251e
Kartikay Awasthi <ikartikay.awasthi@gmail.com>
2025-01-08 10:17:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-green-600 font-bold text-3xl">Hello Namaste, EveryOne lets get code </div>
+  );
+}
+
+export default App;
